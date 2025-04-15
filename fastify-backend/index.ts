7093b24ee8ba4f497bae77eee4a0fe330be8eb23
Avi Udash <udashavi@gmail.com>
2025-04-15 15:33:59 -0700

    setup eslint typescript

--- fastify-backend/index.ts
+++ fastify-backend/index.ts
@@ -6,10 +6,10 @@ server.get('/', async (request, reply) => {
   return { hello: 'world' };
 });
 
-server.listen({port: 8080}, (err, address) => {
-  if(err) {
-    console.error(err)
-    process.exit(1)
+server.listen({ port: 8080 }, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
   }
-  console.log(`Server listening at ${address}`)
-})
+  console.log(`Server listening at ${address}`);
+});
