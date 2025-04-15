5d6562dc0e95b29280adca7a5f6a7fca31a7f4e1
Avi Udash <udashavi@gmail.com>
2025-04-15 16:14:46 -0700

    setup dockerfile

--- fastify-backend/index.ts
+++ fastify-backend/index.ts
@@ -6,7 +6,7 @@ server.get('/', async (request, reply) => {
   return { hello: 'world' };
 });
 
-server.listen({ port: 8080 }, (err, address) => {
+server.listen({ port: 3000, host: '0.0.0.0' }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
