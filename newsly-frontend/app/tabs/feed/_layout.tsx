98d8eb9f9e2cb12429046382333a28cad7ee77e8
Elina Mirbaha <elinamirbaha@gmail.com>
2025-04-30 12:39:21 -0700

    clickable articles, article view

--- newsly-frontend/app/tabs/feed/_layout.tsx
+++ newsly-frontend/app/tabs/feed/_layout.tsx
@@ -1,14 +1,21 @@
 import { Stack } from "expo-router";
 
-export default function feedLayout(){
-    return <Stack>
-
-    <Stack.Screen
-    name="index"
-    options={{
-    headerShown: false,
-    }}
-    />
-
-    </Stack>
-}
+export default function feedLayout() {
+    return (
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="ArticleView"
+          options={{
+            title: "Article Details",
+            presentation: "modal", // gives it a nice zoom/fade effect
+          }}
+        />
+      </Stack>
+    );
+  }
